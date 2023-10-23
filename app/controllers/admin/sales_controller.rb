6233b2d32b9f5059b974cc1f1a9f992f9031adb4
Waleed Ahmed <baloch.waleedahmed@gmail.com>
2023-10-22 20:45:29 -0400

    Now informing user if there is an active sale

--- app/controllers/admin/sales_controller.rb
+++ app/controllers/admin/sales_controller.rb
@@ -1,5 +1,16 @@
 class Admin::SalesController < ApplicationController
+  before_action :authenticate
+
   def index
     @sales = Sale.all
   end
+
+  private
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV['USERNAME'] && password == ENV['PASSWORD']
+    end
+  end
+
 end
